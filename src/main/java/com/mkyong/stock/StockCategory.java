d4263ea5257045d970497e1cb5654898540a364f
williy <willix71@gmail.com>
2016-05-24 08:43:25 +0200

    Initial version with 2 relationtables

---
+++ src/main/java/com/mkyong/stock/StockCategory.java
@@ -0,0 +1,107 @@
+package com.mkyong.stock;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.AssociationOverride;
+import javax.persistence.AssociationOverrides;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "stock_category", schema = "mkyongdb")
+@AssociationOverrides({
+		@AssociationOverride(name = "pk.stock", joinColumns = @JoinColumn(name = "STOCK_ID")),
+		@AssociationOverride(name = "pk.category", joinColumns = @JoinColumn(name = "CATEGORY_ID")) })
+public class StockCategory implements java.io.Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "STOCK_CATEGORY_ID", unique = true, nullable = false)
+	private Integer stockCategoryId;
+	
+	@ManyToOne
+	@JoinColumn(name="STOCK_ID")
+	private Stock stock;
+	
+	@ManyToOne
+	@JoinColumn(name="CATEGORY_ID")
+    private Category category;
+    	
+	@Temporal(TemporalType.DATE)
+	@Column(name = "CREATED_DATE")
+	private Date createdDate;
+	
+	@Column(name = "CREATED_BY", nullable = false, length = 10)
+	private String createdBy;
+	
+	@ManyToMany
+	@JoinTable(name = "stock_category_role", schema = "mkyongdb", 
+		joinColumns={@JoinColumn(name="STOCK_CATEGORY_ID")}, 
+		inverseJoinColumns={@JoinColumn(name="ROLE_ID")})
+	private List<Role> roles;
+	
+	public StockCategory() {
+	}
+
+	public Integer getStockCategoryId() {
+		return stockCategoryId;
+	}
+
+	public void setStockCategoryId(Integer stockCategoryId) {
+		this.stockCategoryId = stockCategoryId;
+	}
+
+	public Stock getStock() {
+		return stock;
+	}
+
+	public void setStock(Stock stock) {
+		this.stock = stock;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public Date getCreatedDate() {
+		return this.createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public String getCreatedBy() {
+		return this.createdBy;
+	}
+	
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+	
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+}
---
+++ src/test/java/com/mkyong/common/AppTest.java
@@ -0,0 +1,50 @@
+package com.mkyong.common;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.mkyong.stock.Role;
+import com.mkyong.stock.Stock;
+import com.mkyong.stock.StockCategory;
+import com.mkyong.util.HibernateUtil;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    
+	@Test
+	public void testSetup() {
+		System.out.println("Hibernate many to many - join table + extra column (Annotation)");
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Assert.assertNotNull(session);
+	}
+	
+	@Test
+	public void testFetch() {
+		System.out.println("Hibernate many to many - join table + extra column (Annotation)");
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		
+		session.beginTransaction();
+		Stock stock1 = (Stock) session.load(Stock.class, 1);
+		List<StockCategory> sc = stock1.getStockCategories();
+		Assert.assertEquals(2, sc.size());
+		
+		StockCategory cat10 = sc.get(0);
+		Assert.assertEquals("CAT10", cat10.getCategory().getName());
+		
+		List<Role> rolesForCat10 = cat10.getRoles();
+		Assert.assertEquals(2, rolesForCat10.size());
+		
+		Role role1 = rolesForCat10.get(0);
+		Assert.assertEquals("role1", role1.getName());
+		
+		Assert.assertEquals(2, role1.getStockCatgeory().size());
+		
+		session.getTransaction().commit();
+	}
+
+}
